15592798064ab2c634098fce1b92af9dcd22365e
Chitesh Goyal <dv.chitesh97@gmail.com>
2017-09-19 09:04:13 -0700

    Add files via upload

---
+++ DEQUEusingDoublyLinkedList 19_9.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+class node
+{
+    public:
+        int data;
+        node* next;
+        node* prev;
+};
+class Dequey{
+public:
+        node* newnode;
+        node* temp;
+        node* head;
+        node* tail;
+        int i,n;
+    void create_node(){
+        node* newnode = new node();
+        cin>>newnode->data;
+        newnode->next = NULL;
+        newnode->prev = NULL;
+    }
+    void init(){
+        head = NULL;
+        tail = NULL;
+        cout<<"Enter Data of First NODE"<<endl;
+        create_node();
+        head = newnode;
+        tail = newnode;
+    }
+    void insertion_beg(){
+        create_node();
+        newnode->next = head;
+        head->prev= newnode;
+        head = head->prev;
+    }
+    void insertion_end(){
+        create_node();
+        newnode->prev = tail;
+        tail->next = newnode;
+        tail = tail->next;
+    }
+    void deletion_beg(){
+        head = head->next;
+    }
+    void deletion_end(){
+        tail = tail->prev;
+        tail->next = NULL;
+    }
+    void display()
+    {
+        temp = head;
+        while(temp != tail){
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+    }
+
+};
+int main(){
+    Dequey d;
+    int choice=0,flag=0;
+    d.init();
+    while(1){
+        cout<<endl<<"1. Insertion at Beg \n2. Insertion at End \n3. Deletion at Beg \n4. Deletion at End \n5. EXIT"<<endl;
+        cin>>choice;
+        if(choice==5)
+            break;
+        switch(choice){
+        case 1:
+            d.insertion_beg();
+            break;
+        case 2:
+            d.insertion_end();
+            break;
+        case 3:
+            d.deletion_beg();
+            break;
+        case 4:
+            d.deletion_end();
+            break;
+        case 5:
+            flag = 1;
+            break;
+        }
+        /*if(flag==1){
+            break;
+        }*/
+    }
+    d.display();
+    return 0;
+}
